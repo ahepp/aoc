5f3ace0d3d10a46e8ac543ca25a0697aa36f3b37
Andrew Hepp <andrew.hepp@ahepp.dev>
2021-12-02 00:19:17 -0800

    Added day 2 in rust.

---
+++ 2021/rs/2/part2.rs
@@ -0,0 +1,35 @@
+use std::fs;
+use std::io;
+use std::io::prelude::*;
+
+fn main() -> io::Result<()> {
+    let file = fs::File::open("input.txt")?;
+    let br = io::BufReader::new(file);
+
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+    let mut aim: i32 = 0;
+    for line in br.lines() {
+        let unwrapped = line.expect("Failed to read line.");
+        let mut split = unwrapped.split(" ");
+        let cmd = split.next().expect("Failed to get command token.");
+        let mag: i32 = split
+            .next()
+            .expect("Failed to get magnitude token.")
+            .parse()
+            .expect("Failed to parse magnitude.");
+        match cmd {
+            "forward" => {
+                x += mag;
+                y += aim * mag;
+            }
+            "up" => aim -= mag,
+            "down" => aim += mag,
+            _ => panic!("Unknown command"),
+        }
+    }
+
+    println!("x: {}\ty: {}\n{}", x, y, x * y);
+
+    Ok(())
+}
