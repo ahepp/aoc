54db844727f68a3db76581f7d5b34232a502132a
Andrew Hepp <andrew.hepp@ahepp.dev>
2021-12-05 23:14:02 -0800

    Use rotate to decrement and reset timers.

--- 2021/rs/6/part2.rs
+++ 2021/rs/6/part2.rs
@@ -2,28 +2,22 @@ use std::fs;
 use std::io;
 
 const RESET_PERIOD: usize = 6;
-const FULL_PERIOD: usize = 8;
+const NEW_PERIOD: usize = 8;
 const SIMULATION_DURATION: u64 = 256;
 
 fn main() -> io::Result<()> {
     let text = fs::read_to_string("input.txt")?;
-    let mut timers: Vec<u64> = vec![0; FULL_PERIOD + 1];
+    let mut timers: [u64; NEW_PERIOD + 1] = [0; NEW_PERIOD + 1];
     for t in text.trim().split(",") {
         let t: usize = t.parse().unwrap();
-        timers[t] = timers[t] + 1;
+        timers[t] += 1;
     }
 
     println!("0:\t{:?}", timers);
-
-    for t in 1..SIMULATION_DURATION+1 {
-        let expired = timers[0];
-        for i in 1..FULL_PERIOD + 1 {
-            timers[i - 1] = timers[i];
-        }
-        timers[RESET_PERIOD] = timers[RESET_PERIOD] + expired;
-        timers[FULL_PERIOD] = expired;
-
-        println!("{}:\t{:?}", t, timers);
+    for t in 0..SIMULATION_DURATION {
+        timers.rotate_left(1);
+        timers[RESET_PERIOD] += timers[NEW_PERIOD];
+        println!("{}:\t{:?}", t + 1, timers);
     }
 
     println!("{}", timers.iter().sum::<u64>());
